c31de5fc2a86ef0cb9a0a360b1ab70a34f907384
raguileoam <raguileoam@gmail.com>
2023-02-11 00:47:42 -0300

    Update ticketService

--- src/services/ticket.service.js
+++ src/services/ticket.service.js
@@ -3,21 +3,18 @@ export default {
     postTicket(office, user) {
         return apiService.post("/api/ticket/", {
             params: {
-                branchOffice: office,
+                branchOffice: {id: office.id},
                 user: { id: user.id }
             }
         })
     },
-    getTickets(user) {
+    getTicketsByUser(user) {
         return apiService.get("/api/ticket/user/", user.id)
     },
-    markAsCancelledTicket(ticket1) {
+    markTicketAsCancelled(ticket1) {
         return apiService.update("/api/ticket/" + ticket1.id + "/mark-as-cancelled");
     },
-    markAsLateTicket(ticket1) {
-        return apiService.update("/api/ticket/" + ticket1.id + "/mark-as-late");
-    },
-    markAsDoneTicket(ticket1) {
+    markTicketAsDone(ticket1) {
         return apiService.update("/api/ticket/" + ticket1.id + "/mark-as-done");
     }
 }
