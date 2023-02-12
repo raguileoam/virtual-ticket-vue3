5acbfe306d2a1fde985aa074e6ba7a67ab989139
raguileoam <raguileoam@gmail.com>
2023-02-12 17:05:15 -0300

    Update AppBar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,47 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+  <v-app-bar color="white" prominent flat>
+    <v-app-bar-nav-icon class="d-sm-none" variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-      Essentials Preset
-    </v-app-bar-title>
+    <v-toolbar-title>Virtual ticket</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <div class="d-none d-sm-flex">
+      <v-btn v-for="url in loggedInUrls" :key="url.value">{{ url.title }}</v-btn>
+    </div>
   </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" location="left" temporary class="d-sm-none" v-if="mobile">
+    <v-list :items="loggedInUrls"></v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup>
-  //
+import { ref, reactive, computed } from "vue";
+import { useDisplay } from "vuetify/lib/framework.mjs";
+const drawer = ref(false);
+const loggedIn = ref(false);
+const urls = reactive([
+  {
+    title: 'Mis tickets',
+    value: '/tickets',
+    needLogin: true
+  },
+  {
+    title: 'Entrar',
+    value: '/login',
+    needLogin: false
+  },
+  {
+    title: 'Salir',
+    value: '/sign-out',
+    needLogin: true
+  },
+]);
+
+const loggedInUrls = computed(() => {
+  return urls.filter(url => url.needLogin == loggedIn.value);
+});
+
+const { mobile } = useDisplay();
 </script>
