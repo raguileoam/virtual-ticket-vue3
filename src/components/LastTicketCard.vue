83b2b7a557c4caa17c9e51fca3f07bd629fa01a7
raguileoam <raguileoam@gmail.com>
2023-02-14 18:19:49 -0300

    Add LastTicketCard

---
+++ src/components/LastTicketCard.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <v-card class="d-flex flex-column align-center" variant="text">
+    <v-card-title>{{ props.title }}</v-card-title>
+    <v-card-text v-if="props.ticket" class="display-3 mb-5">
+      <p class="ticket-number text-h1 d-flex justify-center mb-6">{{ props.ticket }}</p>
+    </v-card-text>
+
+    <v-card-text v-else>
+      <LoadingBar />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup>
+import LoadingBar from './LoadingBar.vue';
+
+const props = defineProps({
+  title: String,
+  ticket: String
+})
+</script>
+
+<style>
+.ticket-number {
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+}
+</style>
